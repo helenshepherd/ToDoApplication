8c930030895d759dd4722ad23dab5fe100533f36
Helen Shepherd <helenaliceshepherd@gmail.com>
2024-04-08 09:57:19 +0100

    typo

--- src/app.test.js
+++ src/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('./app'); 
 
 describe('GET /', () => {
-  it('teste server', async () => {
+  it('test server', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toEqual('SERVER IS UP');
